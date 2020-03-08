2368655ea83994923c703357ff631b9496ac06e1
PundoJnr <pundoarthur@gmail.com>
2020-03-08 06:36:39 +0300

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  { 
   appTitle: string = 'Welcome';
+  appStatus: boolean = true;
+  appList: any[] = [{
+    "ID": "1",
+    "url":"app/images/1.jpg"
+  },{
+    "ID":"2",
+    "url":"app/images/2.jpg"
+  }];
  }
